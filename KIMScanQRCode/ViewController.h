3516ae208cf478f6cb3417bebb32fe98f5d97a03
kim逸云 <844282759@qq.com>
2016-08-07 10:30:12 +0800

    Initial Commit

---
+++ KIMScanQRCode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KIMScanQRCode
+//
+//  Created by 江顺金 on 2016/8/7.
+//  Copyright © 2016年 kim逸云. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ KIMScanQRCode/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  KIMScanQRCode
+//
+//  Created by 江顺金 on 2016/8/7.
+//  Copyright © 2016年 kim逸云. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
